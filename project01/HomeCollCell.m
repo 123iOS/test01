efc91fc782c99e9451f987bf992d901c7300e673
My Repo <tyaa.mail@gmail.com>
2016-03-23 16:35:45 +0700

    Adding Home Page (UICollectionView)

---
+++ project01/HomeCollCell.m
@@ -0,0 +1,17 @@
+//
+//  HomeCollCell.m
+//  project01
+//
+//  Created by Triyakom PT on 3/23/16.
+//  Copyright © 2016 Tyaa. All rights reserved.
+//
+
+#import "HomeCollCell.h"
+
+@implementation HomeCollCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
---
+++ project01/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  project01
+//
+//  Created by Triyakom PT on 3/23/16.
+//  Copyright © 2016 Tyaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
